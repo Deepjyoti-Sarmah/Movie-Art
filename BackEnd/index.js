bfc9c05527ab197778fc09d4edf59b7acbd7287f
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2022-12-04 21:32:13 +0530

    connection between frontend and backend done

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,11 +1,15 @@
 import app from "./server.js"
 import mongodb from "mongodb"
 import ReviewsDAO from "./dao/reviewsDAO.js"
+import dotenv from "dotenv"
+
+dotenv.config({path:'./config.env'});
 
 const MongoClient = mongodb.MongoClient
-const mongo_username = process.env['MONGO_USERNAME']
-const mongo_password = process.env['MONGO_PASSWORD']
-const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.ayr9czp.mongodb.net/?retryWrites=true&w=majority`
+// const mongo_username = process.env['MONGO_USERNAME']
+// const mongo_password = process.env['MONGO_PASSWORD']
+// const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.ayr9czp.mongodb.net/?retryWrites=true&w=majority`
+const uri = process.env.MOVIEART_URI;
 
 const port = 8000
 
